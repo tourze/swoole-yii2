587cfa5ddb9e962e1e1a131b02d2f71d2e6e4348
ywisax <25803471@qq.com>
2016-11-02 21:23:46 +0800

    加载view文件时, 使用eval来替代include, 整体qps可以提升6%

--- src/web/View.php
+++ src/web/View.php
@@ -4,4 +4,30 @@ namespace tourze\swoole\yii2\web;
 
 class View extends \yii\web\View
 {
+
+    /**
+     * @var array
+     */
+    public static $phpCodeCache = [];
+
+    /**
+     * 缓存文件+eval执行
+     *
+     * @inheritdoc
+     */
+    public function renderPhpFile($_file_, $_params_ = [])
+    {
+        ob_start();
+        ob_implicit_flush(false);
+        extract($_params_, EXTR_OVERWRITE);
+
+        if ( ! isset(self::$phpCodeCache[$_file_]))
+        {
+            self::$phpCodeCache[$_file_] = '?>' .file_get_contents($_file_);
+        }
+        //require($_file_);
+        eval(self::$phpCodeCache[$_file_]);
+
+        return ob_get_clean();
+    }
 }
